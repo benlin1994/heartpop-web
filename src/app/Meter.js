b58f86ab3f0a7fadd8dc62448bd20d156262e79c
Benjamin Lin <benlin1994@gmail.com>
2023-10-04 23:15:42 -0700

    progress bar

--- src/app/Meter.js
+++ src/app/Meter.js
@@ -8,7 +8,7 @@ export default function Meter(props) {
             </div>
             <div className="flex border justify-around gap-8">
                 <div className="w-full bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700">
-                    <div className="bg-blue-600 h-1.5 w-1/3 rounded-full dark:bg-blue-500" ></div>
+                    <div style={{width:`${props.points}%`}}className="bg-blue-600 h-1.5 rounded-full dark:bg-blue-500"></div>
                 </div>
             </div>
         </div>
