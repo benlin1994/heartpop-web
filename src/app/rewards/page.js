a6cefaef5a6d16ac3fc2a9d8c85318942b3a2dfb
Benjamin Lin <benlin1994@gmail.com>
2023-10-05 00:01:13 -0700

    fix

---
+++ src/app/rewards/page.js
@@ -0,0 +1,45 @@
+"use client"
+
+import NavBar from '../../components/NavBar'
+import Meter from '../../components/Meter'
+import Feed from '../../components/Feed'
+import items from '../../items.json'
+import { useState } from 'react'
+
+export default function Home() {
+  let [points, setPoints] = useState(0)
+  let initialFeed = new Array(items.length).fill(-1)
+
+
+  let [feedSelection,setFeedSelection] = useState(initialFeed)
+
+  let setFeed = (feedIndex,choiceIndex) => {
+    let newFeedSelection = [...feedSelection]
+    newFeedSelection[feedIndex] = choiceIndex
+    console.log(newFeedSelection)
+    setFeedSelection(newFeedSelection)
+  }
+
+  let calcPoints = (feedSelection) => {
+    let total = 0
+    feedSelection.forEach((val, i ) => {
+      if(val !== -1){
+        total += 5
+      }
+    })
+    console.log(total)
+    return total
+  }
+ 
+
+  return (
+    <div className='bg-yellow-300'>
+      <Meter points={calcPoints(feedSelection)}/>
+      <div className='mt-20'>
+        <Feed items={items} setFeed={setFeed} feedSelection={feedSelection}/>
+      </div>
+      
+      <NavBar />
+    </div>
+  )
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,52 +3,56 @@ import ShopIcon from "../icons/ShopIcon"
 import RewardsIcon from "../icons/RewardsIcon"
 import ActivityIcon from "../icons/ActivityIcon"
 import PlayIcon from "../icons/PlayIcon"
+import { usePathname } from 'next/navigation'
+import Link from "next/link"
 
 export default function NavBar() {
+
+    let path = usePathname()
 
     return (
         <div className="fixed bottom-0 left-0 right-0">
             <div className="flex border bg-blue-400 text-center justify-around content-center p-4 gap-8">
-            <   div className="w-8 m-auto">
+                <Link href="/" className="w-8 m-auto fill-white">
                     <div className="flex justify-center">
-                        <PlayIcon/>
+                        <PlayIcon isActive={path === "/"}/>                        
                     </div>
                     <label className="flex justify-center">
                         Play
                     </label>
-                </div>
-                <div className="w-8 m-auto">
+                </Link>
+                <Link href="/activity" className="w-8 m-auto">
                     <div className="flex justify-center">
-                        <ActivityIcon/>
+                        <ActivityIcon isActive={path === "/activity"}/>
                     </div>
                     <label className="flex justify-center">
                         Activity
                     </label>
-                </div>
-                <div className="w-8 m-auto">
+                </Link>
+                <Link href="/profile" className="w-8 m-auto">
                     <div className="flex justify-center">
-                        <ProfileIcon/>
+                        <ProfileIcon isActive={path === "/profile"}/>
                     </div>
                     <label className="flex justify-center">
                         Profile
                     </label>
-                </div>
-                <div className="w-8 m-auto">
+                </Link>
+                <Link href="/rewards" className="w-8 m-auto">
                     <div className="flex justify-center">
-                        <RewardsIcon/>
+                        <RewardsIcon isActive={path === "/rewards"}/>
                     </div>
                     <label className="flex justify-center">
                         Rewards
                     </label>
-                </div>
-                <div className="w-8 m-auto">
+                </Link>
+                <Link href="/shop" className="w-8 m-auto">
                     <div className="flex justify-center">
-                        <ShopIcon/>
+                        <ShopIcon isActive={path === "/shop"}/>
                     </div>
                     <label className="flex justify-center">
                         Shop
                     </label>
-                </div>
+                </Link>
             </div>
         </div>
         )
