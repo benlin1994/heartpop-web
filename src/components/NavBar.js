b228d257a2586cbefe85ed63247461e99fb8eef4
Benjamin Lin <benlin1994@gmail.com>
2023-10-04 23:41:26 -0700

    fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,12 +8,12 @@ export default function NavBar() {
 
     return (
         <div className="fixed bottom-0 left-0 right-0">
-            <div className="flex border bg-blue-400 justify-around content-center p-4 gap-8">
+            <div className="flex border bg-blue-400 text-center justify-around content-center p-4 gap-8">
             <   div className="w-8 m-auto">
                     <div className="flex justify-center">
                         <PlayIcon/>
                     </div>
-                    <label>
+                    <label className="flex justify-center">
                         Play
                     </label>
                 </div>
@@ -21,7 +21,7 @@ export default function NavBar() {
                     <div className="flex justify-center">
                         <ActivityIcon/>
                     </div>
-                    <label>
+                    <label className="flex justify-center">
                         Activity
                     </label>
                 </div>
@@ -29,7 +29,7 @@ export default function NavBar() {
                     <div className="flex justify-center">
                         <ProfileIcon/>
                     </div>
-                    <label>
+                    <label className="flex justify-center">
                         Profile
                     </label>
                 </div>
@@ -37,7 +37,7 @@ export default function NavBar() {
                     <div className="flex justify-center">
                         <RewardsIcon/>
                     </div>
-                    <label>
+                    <label className="flex justify-center">
                         Rewards
                     </label>
                 </div>
@@ -45,7 +45,7 @@ export default function NavBar() {
                     <div className="flex justify-center">
                         <ShopIcon/>
                     </div>
-                    <label>
+                    <label className="flex justify-center">
                         Shop
                     </label>
                 </div>
