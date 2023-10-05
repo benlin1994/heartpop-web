bc6e624afed75baa986308e48fbd9101cd2806bb
Benjamin Lin <benlin1994@gmail.com>
2023-10-05 01:20:00 -0700

    percs

--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -24,28 +24,31 @@ export default function FeedItem({setFeed,feedIndex,choices,question,answer, fee
             {
                 choices.map((choice, index) => {
                     let sum = 100
-                    const numbers = []
-                    for (let i = 0; i < choices.length; i++) {
-                        const randomNumber = Math.floor(Math.random() * sum)
-                        sum -= randomNumber < 0 ? 0 : randomNumber
-                        numbers.push(randomNumber < 0 ? 0 : randomNumber)
+                    let total = 0
+                    const tempNumbers = []
+                    for (let i = 0; i < 3; i++) {
+                        let randomNumber = Math.floor(Math.random() * sum)
+                        tempNumbers.push(randomNumber)
+                        total += randomNumber
                     }
-                    numbers.push(sum)
+                    tempNumbers.push(sum)
+                    let numbers = tempNumbers.map(x => Math.floor(((x * 100)/tempNumbers.reduce((a, b) => a + b, 0))))
+                    let grand = tempNumbers.reduce((a, b) => a + b, 0)
 
                     if(index === feedSelection[feedIndex]){
                         return (
                             <div onClick={()=>setFeed(feedIndex, index)} 
                                 className="bg-yellow-200 border rounded p-1" key={index}
-                                style={{background:`linear-gradient(to right, pink ${numbers[index]}%, #07aa91 ${numbers[index]}%, #07aa91)`}}
+                                style={{background:`linear-gradient(to right, pink ${numbers[index]}%, #D65780 ${numbers[index]}%, #D65780)`}}
                                 >
-                                {choice} - {numbers[index]}%
+                                {choice} - {numbers[index]} %
                             </div>
                         )
                     }else if(feedSelection[feedIndex] !== -1){
                         return (
                             <div onClick={()=>setFeed(feedIndex, index)} 
                                 className="bg-yellow-200 border rounded p-1" key={index}
-                                style={{background:`linear-gradient(to right, #a2ea4c ${numbers[index]}%, #07aa91 ${numbers[index]}%, #07aa91)`}}
+                                style={{background:`linear-gradient(to right, #a2ea4c ${numbers[index]}%, green ${numbers[index]}%, green)`}}
                                 >
                                 {choice} - {numbers[index]}%
                             </div>
